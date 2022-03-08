7a8e4a5f5b1e6d0b602becfc33e262d597f9041a
devogs <96696291+devogs@users.noreply.github.com>
2022-03-08 22:51:26 +0100

    Update k8s_install.sh

--- k8s_install.sh
+++ k8s_install.sh
@@ -4,7 +4,7 @@
 sudo apt update
 sudo apt -y install curl apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.lis
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt update
 sudo apt -y install vim git curl wget kubelet kubeadm kubectl
