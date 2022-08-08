7e4377ae074986e1effeb987385db5b110136a48
devogs <96696291+devogs@users.noreply.github.com>
2022-08-08 20:25:27 +0200

    Update primary.sh

--- primary.sh
+++ primary.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-apt update && apt upgrade -y
-apt install net-tools -y
-apt-get install openssh-server -y
-systemctl enable ssh
-systemctl start ssh
+#apt update && apt upgrade -y
+#apt install net-tools -y
+#apt-get install openssh-server -y
+#systemctl enable ssh
+#systemctl start ssh
 #apt install ansible -y
 #install docker
 apt-get install \
